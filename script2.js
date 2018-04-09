749b8a30e8f45443cfb3ee2ca2013922d6901134
Yavuz <yavuz.orbey@gmail.com>
2018-04-09 13:21:41 -0400

    Add basic arithmetic functions

--- script2.js
+++ script2.js
@@ -1,9 +1,13 @@
+const MAX_LENGTH = 16;
+
 var buttonsTag = document.getElementById("buttons");
-var array = [7,8,9,'/',4,5,6,'X',1,2,3,'-', 0,'.','=','+'];
-let current = 0;
+var array = [7,8,9,'/',4,5,6,'*',1,2,3,'-', 0,'.','=','+'];
+var current=0, previous, temp;
+let operation, reset = false;
 
 
 Calculator();
+
 function Calculator(){
 	initializeButtons();
 
@@ -25,28 +29,76 @@ for(i=0; i<buttons.length; i++){
 		
 		switch(button){
 			case '+':
-
-				var number = parseInt(display.innerText);
-				display.innerHTML = "";
-				current+=number;
-				break;
 			case '-':
-				current -=parseInt(display.innerText);
-				break;
 			case '/':
 			case '*':
-				
+				if(operation){
+					console.log("Hi");
+					calculate(previous, current, operation);
+				}
+				previous = parseInt(display.innerText);
+				operation = button;
+				display.innerText = "";
 				break;
 			case '=':
 				display.innerText = current;
+				reset = true;
 				break;
 			default:
+				if(placeValue(parseInt(display.innerText))<MAX_LENGTH){
+					if(reset){
+						display.innerText = "";
+
+					}
+
 				display.innerText+=button;
+				current = parseInt(display.innerText);
+				current = calculate(previous, current, operation);
+				}
 		}
 	}
 }
 }
 
+function calculate(previous, current, operation){
+	switch(operation){
+		case '+':
+		
+			temp = previous;
+			previous = current;
+			current+=temp;
+			return current;
+			break;
+		case '-':
+			temp = previous;
+			previous = current;
+			current=temp-current;
+			return current;
+			break;
+		case '/':
+			temp = previous;
+			previous = current;
+			current=Math.floor(temp/current);
+			return current;
+			break;
+		case '*':
+			temp = previous;
+			previous = current;
+			current=Math.floor(temp*current);
+			return current;
+			break;
+		default:
+			console.log('No operation set yet');
+	}
+}
+function placeValue(number){
+	let count =1;
+	while(Math.floor(number/10)>0){
+		number = Math.floor(number/10);
+		count++;
+	}
+	return count;
+}
 
 
 
